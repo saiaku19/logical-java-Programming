28a86d155c3e778b61d580cc5a20ec67873def77
saiaku19 <kalakotiakankshasai@gmail.com>
2024-11-12 23:31:20 +0530

    Add files via upload

---
+++ day-3/Break.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Break
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=1;i<=10;i++)
+        {
+            if(i==9)
+            {
+                break;
+            }
+            System.out.println("The numbers are:"+i);
+        }
+    }
+
+}
